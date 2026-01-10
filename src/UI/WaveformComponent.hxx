adc87bdfd60bcf14764fb6babd0af17421e31934
josura <giorgiolocicero@hotmail.com>
2026-01-10 22:20:07 +0100

    feature: commented old paint function to visualize the waveform to also consider playhead

--- src/UI/WaveformComponent.hxx
+++ src/UI/WaveformComponent.hxx
@@ -14,20 +14,43 @@ public:
         thumbnail.setSource(new juce::FileInputSource(file));
     }
 
+    void setPlayheads(const std::vector<float>& positions) {
+        currentPositions = positions;
+        repaint();
+    }
+
+    // void paint(juce::Graphics& g) override {
+    //     g.fillAll(juce::Colours::black);
+    //     g.setColour(juce::Colours::cyan);
+
+    //     if (thumbnail.getNumChannels() > 0) {
+    //         thumbnail.drawChannels(g, getLocalBounds(), 0.0, thumbnail.getTotalLength(), 1.0f);
+    //     } else {
+    //         g.drawText("Drop an audio file here", getLocalBounds(), juce::Justification::centred);
+    //     }
+    // }
     void paint(juce::Graphics& g) override {
+        // Draw Waveform (Existing code)
         g.fillAll(juce::Colours::black);
         g.setColour(juce::Colours::cyan);
-
-        if (thumbnail.getNumChannels() > 0) {
+        if( thumbnail.getNumChannels() > 0 ){
             thumbnail.drawChannels(g, getLocalBounds(), 0.0, thumbnail.getTotalLength(), 1.0f);
         } else {
             g.drawText("Drop an audio file here", getLocalBounds(), juce::Justification::centred);
+        }
+
+        // Draw the "Dust" (Multiple Playheads), to be tested
+        g.setColour(juce::Colours::white.withAlpha(0.6f)); // Semi-transparent
+        for (float pos : currentPositions) {
+            auto x = pos * getWidth();
+            g.drawLine(x, 0, x, getHeight(), 1.5f);
         }
     }
 
     void changeListenerCallback(juce::ChangeBroadcaster*) override { repaint(); }
 
 private:
+    std::vector<float> currentPositions;
     juce::AudioThumbnailCache thumbnailCache{ 5 };
     juce::AudioThumbnail thumbnail;
 };
