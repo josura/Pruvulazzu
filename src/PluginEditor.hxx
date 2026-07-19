ed00be385ce13698bcb2d04f947325d7c8c2838e
josura <giorgiolocicero@hotmail.com>
2026-07-19 16:04:48 +0200

    documentation+feature: added doxygen for plugin editor along first declaration of additional sliders for start and end of the interval to draw grains

--- src/PluginEditor.hxx
+++ src/PluginEditor.hxx
@@ -4,6 +4,17 @@
 #include "DSP/FileLoader.hxx"
 #include "UI/WaveformComponent.hxx"
 
+/**
+ * @brief The main editor class for the Pruvulazzu audio plugin.
+ * This class is responsible for the graphical user interface of the plugin, including parameter controls and waveform visualization. 
+ * It inherits from juce::AudioProcessorEditor, juce::FileDragAndDropTarget, and juce::Timer to handle audio processing, file drag-and-drop functionality, and periodic updates respectively.
+ * It also manages the synchronization between the UI controls and the underlying audio processor parameters using juce::AudioProcessorValueTreeState::SliderAttachment.
+ * The included sliders for now are:
+ * - Density: Controls the density of the grains (how many grains are played per second).
+ * - Size: Controls the size of the grains.
+ * - Start: Controls the start position of the sample playback(where to draw the grains from, along with the end).
+ * - End: Controls the end position of the sample playback.
+ */
 class PruvulazzuAudioProcessorEditor : public juce::AudioProcessorEditor,
                                    public juce::FileDragAndDropTarget,
                                    public juce::Timer
@@ -30,13 +41,19 @@ private:
 
     juce::Label densityLabel;
     juce::Label sizeLabel;
+    juce::Label startLabel;
+    juce::Label endLabel;
 
     juce::Slider densitySlider;
     juce::Slider sizeSlider;
-    
+    juce::Slider startSlider;
+    juce::Slider endSlider;
+
     // These "Attachments" automatically sync the UI knobs to the processor parameters
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> densityAttachment;
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> sizeAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> startAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> endAttachment;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PruvulazzuAudioProcessorEditor)
 };
